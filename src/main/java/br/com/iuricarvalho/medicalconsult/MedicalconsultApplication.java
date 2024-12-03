fb3819713670ea5caf723702141cd53b6c244e9d
iuricarvalho302 <iuricarvalho302@gmail.com>
2024-12-02 22:26:11 -0300

    segundo

---
+++ src/main/java/br/com/iuricarvalho/medicalconsult/MedicalconsultApplication.java
@@ -0,0 +1,13 @@
+package br.com.iuricarvalho.medicalconsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class git aMedicalconsultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalconsultApplication.class, args);
+	}
+
+}
